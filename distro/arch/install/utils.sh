dcb32074af0ef149895b9535bbb331b304dc64b1
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-11-10 20:24:22 +0100

    install: utils library

---
+++ distro/arch/install/utils.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+install_package() {
+  local command="$1"
+  local package_name="${2:-"$command"}"
+
+  if command -v $command >/dev/null 2>&1; then
+    echo "INFO: $package_name is already available, skipping"
+  else
+    sudo pacman -Sy --needed --noconfirm $package_name
+
+    if [[ $? -ne 0 ]]; then
+      echo "ERROR: error while installing $package_name"
+      return 1
+    fi
+
+    echo "SUCCESS: $package_name was installed successfully"
+  fi
+
+  return 0
+}
