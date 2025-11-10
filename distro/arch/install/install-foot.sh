01de053d0f78c5bc20ccc8d119edef5a86b9b497
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-11-10 20:31:33 +0100

    install: foot install script

---
+++ distro/arch/install/install-foot.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+
+pushd "$(dirname "$(realpath ${BASH_SOURCE[0]})")"
+. ./utils.sh
+
+install_package foot
+
+if [[ $? -e 0 ]]; then
+  systemctl --user enable --now foot-server.service
+fi
+
+popd
