b59d9225bba780a2f5181b8bac2498d555fc62ae
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-11-10 20:32:12 +0100

    install: xdg stuff install script

---
+++ distro/arch/install/install-xdg.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+pushd "$(dirname "$(realpath ${BASH_SOURCE[0]})")"
+. ./utils.sh
+
+install_package xdg-desktop-portal
+install_package xdg-user-dirs
+install_package xdg-utils
+install_package xdg-terminal-exec
+
+popd
+
+xdg-user-dirs-update
