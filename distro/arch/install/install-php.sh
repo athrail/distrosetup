2cebf3562885d0c98e194c934b4b2a97516880c1
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-11-10 22:40:14 +0100

    install: adding various PHP modules

--- distro/arch/install/install-php.sh
+++ distro/arch/install/install-php.sh
@@ -4,5 +4,14 @@ pushd "$(dirname "$(realpath ${BASH_SOURCE[0]})")"
 . ./../utils.sh
 
 install_package php
+install_package composer
+
+install_package php-fpm
+install_package php-gd
+install_package php-pgsql
+install_package php-redis
+install_package php-sodium
+install_package php-sqlite
+install_package php-xsl
 
 popd
