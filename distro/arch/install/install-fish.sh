d130f4172355f4adfff70875a9cbda5a0b6c9359
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-11-10 19:53:57 +0100

    install: fish shell install script

---
+++ distro/arch/install/install-fish.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+pushd "$(dirname "$(realpath ${BASH_SOURCE[0]})")"
+. ./utils.sh
+
+install_package fish
+
+echo "INFO: setting fish as default shell"
+chsh -s /usr/bin/fish
+
+popd
